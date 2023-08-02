f70faf54b398b11b10d93b98f98dceed625e1cff
uosjapuelks <anderson@u.nus.edu>
2023-08-02 21:26:41 +0800

    fixed height overflow

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,8 +1,8 @@
 import {FixedCard, DynamicCard} from "../components/portfolio";
-import ee2026 from "../images/portfolio/EE2026/BoardIntro.jpg";
-import cg2271 from "../images/portfolio/CG2271/2271Intro.png";
-import cg4002 from "../images/portfolio/CG4002/CG4002Group.png";
-import fridget from "../images/portfolio/Fridget/FridgetIntro.jpg";
+import ee2026 from "../images/portfolio/covers/BoardIntro.jpg";
+import cg2271 from "../images/portfolio/covers/2271Intro.png";
+import cg4002 from "../images/portfolio/covers/CG4002Group.png";
+import fridget from "../images/portfolio/covers/FridgetIntro.jpg";
 
 //TODO: Each portfolio's details
 export const Portfolio = (props) => {
